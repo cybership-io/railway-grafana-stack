a2ba51be0b0b9b146bdbf9b5faf63d323671cac0
Muritala David <davmuri1414@gmail.com>
2025-09-12 13:31:23 +0100

    load grafana dashboard from s3 bucket

--- grafana/download-dashboards.sh
+++ grafana/download-dashboards.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p /etc/grafana/provisioning/dashboards/files
 
-DASHBOARDS="api-debugging api-key-metrics app-overview business-metrics database-connections database-metrics integrations-metrics queue-metrics system-health-overview error-debugging performance-debugging redis-metrics"
+DASHBOARDS="api-debugging api-key-metrics app-overview business-metrics database-connections database-metrics integrations-metrics queue-metrics system-health-overview error-debugging redis-metrics"
 BASE_URL="https://s3.us-east-1.amazonaws.com/cdn.cybership.dev/observability/dashboards/json"
 
 for dashboard in $DASHBOARDS; do
